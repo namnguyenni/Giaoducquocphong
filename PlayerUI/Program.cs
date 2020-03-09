f19a76d96c4b6a7105a762ee0ece0d8c251c84dd
Hoàng Tùng <54903350+hoangtung97@users.noreply.github.com>
2020-03-09 16:08:54 +0700

    COMMIT

--- PlayerUI/Program.cs
+++ PlayerUI/Program.cs
@@ -8,15 +8,13 @@ namespace PlayerUI
 {
     static class Program
     {
-        /// <summary>
-        /// Punto de entrada principal para la aplicación.
-        /// </summary>
+      
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form4());
         }
     }
 }
